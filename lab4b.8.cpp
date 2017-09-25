d9b019a5f24a983f3383446a7d907c8b7b329328
Rimilmandrita <31147874+Rimilmandrita@users.noreply.github.com>
2017-09-25 10:20:15 +0530

    Update lab4b.8.cpp

--- lab4b.8.cpp
+++ lab4b.8.cpp
@@ -1,1 +1,23 @@
 
+#include <iostream>
+using namespace std;
+
+int s_dig(int num,int sum){
+while (num>0){
+int dig=num%10;
+sum=sum+dig;
+num=num/10;
+s_dig(num,sum);
+}
+return sum;
+}
+
+int main() {
+int num,sum=0;
+cout<<"Enter any number.";
+cin>>num;
+cout<<"\nThe number entered is "<<num;
+sum=s_dig(num,sum);
+cout<<"\nThe sum of digits is "<<sum;
+	return 0;
+}

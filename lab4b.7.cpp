371cdc3a7ac488dfa691469c20f41166e75bb0ea
Rimilmandrita <31147874+Rimilmandrita@users.noreply.github.com>
2017-09-25 10:13:57 +0530

    Update lab4b.7.cpp

--- lab4b.7.cpp
+++ lab4b.7.cpp
@@ -1,1 +1,27 @@
 
+#include <iostream>
+using namespace std;
+
+int reverse(int num,int rev){
+while (num>0){
+int dig=num%10;
+rev=rev*10+dig;
+num=num/10;
+reverse(num,rev);
+}
+return rev;
+}
+
+int main() {
+int num,rev=0,rnum;
+cout<<"Enter any number.";
+cin>>num;
+int n=num;
+cout<<"\nThe number entered is "<<num;
+rnum=reverse(num,rev);
+if(rnum==n)
+{cout<<"\nThe number is a palindrome.";}
+else
+cout<<"\nThe number is not a palindrome.";
+	return 0;
+}

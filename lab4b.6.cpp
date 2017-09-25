9f7e5d914f3caa4bef24a430b63ff425c6902cd3
Rimilmandrita <31147874+Rimilmandrita@users.noreply.github.com>
2017-09-25 09:27:30 +0530

    Update lab4b.6.cpp

--- lab4b.6.cpp
+++ lab4b.6.cpp
@@ -1,1 +1,22 @@
 
+#include <iostream>
+using namespace std;
+
+int rev(int num){
+int dig;
+if(num>0){
+dig=num%10;
+cout<<dig;
+rev(num/10);
+}
+return 0;
+}
+
+int main() {
+int num;
+cout<<"Enter any number.";
+cin>>num;
+cout<<"\nThe reversed number is - ";
+rev(num);
+	return 0;
+}

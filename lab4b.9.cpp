feb4c27c17eedb2789c2f309aff95c8ad6ffa51d
Rimilmandrita <31147874+Rimilmandrita@users.noreply.github.com>
2017-09-25 10:32:53 +0530

    Create lab4b.9.cpp

---
+++ lab4b.9.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+long int fact(int num,long int prod)
+{
+while (num>0){
+prod=prod*num;
+num--;
+fact(num,prod);
+}
+return prod;
+}
+
+int main() {
+int num;
+long int prod=1;
+cout<<"Enter any number.";
+cin>>num;
+cout<<"\nThe number entered is "<<num;
+prod=fact(num,prod);
+cout<<"\nThe factorial of number is "<<prod;
+	return 0;
+}

f5f3e1f1fe5e67f6212fe90a225b946101201fd8
Rimilmandrita <31147874+Rimilmandrita@users.noreply.github.com>
2017-09-30 23:39:02 +0530

    Update Lab4b.12.cpp

--- Lab4b.12.cpp
+++ Lab4b.12.cpp
@@ -1,1 +1,24 @@
 
+#include <iostream>
+using namespace std;
+
+int LCM(int a, int b)
+{static int temp=0;
+temp+=b;
+{if(temp%a==0 && temp%b==0 )
+return temp;
+else
+ return LCM(a,b);}
+}
+ 
+int main()
+{ int a,b,lcm;
+cout<<"Enter two numbers whose LCM is to be found.";
+cin>>a>>b;
+if(a > b)
+        lcm =LCM(b, a);
+    else
+        lcm = LCM(a,b);
+cout<<"\nThe LCM of "<<a<<" & "<<b<< " is "<<lcm;
+	return 0;
+}
